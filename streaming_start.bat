4d7272dba6b8f86c71c8d2ca35350ca67322abb8
jbaylies <jbaylies@eventide.com>
2024-07-10 08:18:02 -0400

    init

---
+++ streaming_start.bat
@@ -0,0 +1,10 @@
+@echo off
+cd "C:\Users\jbayl\Music\H9K_to_OBS_mastering Project"
+start "" "H9K_to_OBS_mastering.als"
+cd "C:\Program Files\Sonarworks\SoundID Reference\Systemwide"
+start "" "SoundID Reference.exe"
+cd "C:\Users\jbayl\Documents\Max 8\Projects\max_gamepad_xbox_input_display\gamepad-input-display"
+start "" "gamepad-input-display.maxproj"
+cd "C:\Program Files\obs-studio\bin\64bit"
+start "" "obs64.exe"
+exit
